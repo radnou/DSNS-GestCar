1c26dbc4ecd550d22fb8b2c1e3e9f115db262d58
Radnoumane MOSSABELY <mossabelyradnoumane@gmail.com>
2024-09-16 20:40:59 +0200

    refactor: remove commented out code in App.java

--- demo/src/main/java/com/rmoss/gestcar/App.java
+++ demo/src/main/java/com/rmoss/gestcar/App.java
@@ -24,7 +24,7 @@ public class App {
 
         // Afficher toutes les voitures
         System.out.println("Liste des voitures disponibles :");
-        gestionVoitures.afficherVoitures();       
+        gestionVoitures.afficherVoitures();
 
         // Création d'un client
         Client client = new Client("Emmanuel Macron", "123 Rue des Fleurs", "0123456789");
@@ -39,19 +39,19 @@ public class App {
         // Afficher l'historique des locations
         System.out.println("\nHistorique des locations:");
         gestionVoitures.afficherHistoriqueLocations();
-        // Ajouter un client depuis la console
-    gestionVoitures.ajouterClientDepuisConsole();
-
-    // Louer une voiture depuis la console
-    Client client2 = gestionVoitures.getClients().get(0); // Sélection du premier client pour simplifier
-
-    // Ajouter et afficher des voitures comme avant, puis louer depuis la console
-    VoitureThermique voiture = new VoitureThermique("Renault", "Clio", 2020, 50.0, 6.2);
-    gestionVoitures.ajouterVoiture(voiture);
-
-    LocalDate dateDebut = gestionVoitures.saisirDateDepuisConsole("Entrez la date de début (yyyy-MM-dd) : ");
-    LocalDate dateFin = gestionVoitures.saisirDateDepuisConsole("Entrez la date de fin (yyyy-MM-dd) : ");
-
-    gestionVoitures.louerVoiture(voiture, client, dateDebut, dateFin);
+//        // Ajouter un client depuis la console
+//        gestionVoitures.ajouterClientDepuisConsole();
+//
+//        // Louer une voiture depuis la console
+//        Client client2 = gestionVoitures.getClients().get(0); // Sélection du premier client pour simplifier
+//
+//        // Ajouter et afficher des voitures comme avant, puis louer depuis la console
+//        VoitureThermique voiture = new VoitureThermique("Renault", "Clio", 2020, 50.0, 6.2);
+//        gestionVoitures.ajouterVoiture(voiture);
+//
+//        LocalDate dateDebut = gestionVoitures.saisirDateDepuisConsole("Entrez la date de début (yyyy-MM-dd) : ");
+//        LocalDate dateFin = gestionVoitures.saisirDateDepuisConsole("Entrez la date de fin (yyyy-MM-dd) : ");
+//
+//        gestionVoitures.louerVoiture(voiture, client, dateDebut, dateFin);
     }
 }
